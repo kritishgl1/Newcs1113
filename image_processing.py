31d2e8b0a12b3dffdb7c3ec186f7b9f76bd0d5b3
Kriti Sehgal <kritishgl1 @gmail.com>
2015-11-30 09:48:11 -0500

    first commit

---
+++ image_processing.py
@@ -0,0 +1,53 @@
+import pygame as pg
+
+# grayPixel: pixel -> pixel
+# compute and return a gray pixel with the same intensity
+# as the given pixel
+def grayPixel(pixel):
+    red_intensity = pixel[0]
+    green_intensity = pixel[1]
+    blue_intensity = pixel[2]
+    ave_intensity = (red_intensity + green_intensity+ blue_intensity)//3
+    return (ave_intensity, ave_intensity, ave_intensity)
+
+# channel: pixel -> channel -> pixel
+# return a gray pixel with intensity from given channel of given pixel
+def channel(pixel,chan):
+    return (pixel[chan],pixel[chan],pixel[chan])
+
+
+# inverse: pixel -> pixel
+# return the color negative of the given pixel
+def inverse(pixel):
+    return (255-pixel[0], 255-pixel[1], 255-pixel[2])
+
+
+# intensify: pixel -> nat255 -> pixel
+# brighten each channel of pixel by quantity
+def intensify(pixel,quantity):
+    return (pixel[0]+quantity, pixel[1]+quantity, pixel[2]+quantity)
+
+# lighten: pixel -> pixel
+# return a ligher image by adding 10 to each intensity level
+def lighten(pixel):
+    return (pixel[0]+10, pixel[1]+10, pixel[2]+10)
+
+
+# darken: pixel -> pixel
+# return a darker image by subtracting 10 from each intensity level
+def darken(pixel):
+    return (pixel[0]-10, pixel[1]-10, pixel[2]-10)
+
+def invert(image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = inverse(pixels3d[x,y])
